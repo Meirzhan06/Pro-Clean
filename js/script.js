8bdfc468b2558a04be6ca5dc35373f52253014b9
Anonim <103999938+Meirzhan06@users.noreply.github.com>
2022-12-06 12:29:09 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
     $('.bars').click(function(event) {
         $('.burger-menu').toggleClass('active');
+        $('body').toggleClass('lock');
     });
 });
 
 $(document).ready(function() {
     $('.button-close-burger').click(function(event) {
         $('.burger-menu').removeClass('active');
+        $('body').removeClass('lock');
     });
 });
